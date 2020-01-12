2fb897935f63f91641630cac9ae8262e77946c5e
asleniovas <asleniovas@gmail.com>
2020-01-12 18:49:19 +0000

    additional text cleaning

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ import nltk
 
 #nltk.download('all')
 
-#3 ~ for generating word clouds
+#3 for quick string manipulations
+import string
+
+#4 ~ for generating word clouds
 from wordcloud import WordCloud
 
-#4 ~ for image processing with WordCloud
+#5 ~ for image processing with WordCloud
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-#5 ~ our classes from other files that hold various text cleaning and manipulation methods
+#6 ~ our classes from other files that hold various text cleaning and manipulation methods
 from textCleaner import TextCleaner
 from textCounter import TextCounter
 
@@ -46,30 +49,35 @@ stop_words_list = stop_words_list + new_stop_words
 # |   STEP 1 - TEXT FILE LOCATION AND OPENING   |
 # |                                             |
 
-#set text file location and open, mine is in Documents
-repo = os.path.join(os.path.expanduser("~"), "Documents/repos/keywordAnalysis")
-file_path = os.path.join(repo, "Apple_Event_2019_09.txt")
 
-file_open = open(file_path, encoding="utf-8-sig")
+text_files = ["Apple_Event_2017_09.txt", "Apple_Event_2018_09.txt", "Apple_Event_2019_09.txt"]
 
+for single_file in range(1, len(text_files)):
+
+    #set text file location and open, mine is in Documents
+    repo = os.path.join(os.path.expanduser("~"), "Documents/repos/keywordAnalysis")
+    file_path = os.path.join(repo, text_files[single_file])
+
+    file_open = open(file_path, encoding="utf-8-sig").read()
 
 # |_                                            _|
 # |   STEP 2 - TEXT CLEANING AND NORMALISATION   |
 # |                                              |
 
-#convert to lowercase and split text into words
-file_strings = file_open.read().lower().split()
+    #remove punctiation, convert to lowercase, and split words
+    text = file_open.translate(str.maketrans("", "", string.punctuation))
+    file_strings = text.lower().split()
 
-#close the file after producing required array
-file_open.close()
+    #close the file after producing required array
+    file_open.close()
 
-#clean words from stop words by calling compareRemove() function
-cleaner_class = TextCleaner()
-updated_file_strings = cleaner_class.compareRemove(stop_words_list, file_strings)
+    #clean words from stop words by calling compareRemove() function
+    cleaner_class = TextCleaner()
+    updated_file_strings = cleaner_class.compareRemove(stop_words_list, file_strings)
 
-#producing a dictionary with word occurrence counts
-counter_class = TextCounter()
-string_dictionary = counter_class.countElements(updated_file_strings)
+    #producing a dictionary with word occurrence counts
+    counter_class = TextCounter()
+    string_dictionary = counter_class.countElements(updated_file_strings)
 
 # |_                                       _|
 # |    STEP X - WORD CLOUD VISUALISATION    |
